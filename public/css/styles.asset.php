a63e141e56e6b176be0ac7e5f0941f4a8a711af9
HadaRaion <sweet.raion@gmail.com>
2025-10-26 15:47:39 -0700

    Remove public folder from git tracking

--- public/css/styles.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '570eb9f0ed41bdb5bb11');
--- public/js/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '79ef346ad1a861e7470f');
