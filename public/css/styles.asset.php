bea335792a6d336af5bb7427551787041e019e5e
HadaRaion <sweet.raion@gmail.com>
2025-10-31 23:17:33 -0700

    Add build

---
+++ public/css/styles.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '637f7008c5e2e6f2124b');
---
+++ public/js/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '41c4d3a98f7f06997c36');
